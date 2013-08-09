e0885b1ee0cb96e89c96521a9c5ec6b4d8fb653d
kalmanb <kalman@movio.co>
2013-08-10 01:32:03 +1200

    Clean up remote train interface

--- src/main/scala/Remote.scala
+++ src/main/scala/Remote.scala
@@ -8,22 +8,24 @@ object Remote extends App {
   val BaseUrl = "http://icfpc2013.cloudapp.net/"
   val params = Map("auth" -> "0328SAmjHevv7SW0OT2lYdIxjSuhVp2HX31j1dSSvpsH1H")
 
+  case class TrainingRequest(
+    size: Option[Int],
+    operators: Option[Seq[String]])
+
   case class TrainingProblem(
     challenge: String,
     id: String,
     size: Int,
     operators: Seq[String])
 
-  def getTrainer(size: Option[Int] = None, operators: Option[Seq[String]] = None): TrainingProblem = {
-    implicit val resultFormatter = jsonFormat4(TrainingProblem)
-    case class TrainingRequest(
-      size: Option[Int],
-      operators: Option[Seq[String]])
+  def getTrainer(size: Option[Int] = None, operators: Option[Seq[String]] = None): TrainingProblem =
+    getTrainer(TrainingRequest(size, operators))
+
+  def getTrainer(request: TrainingRequest): TrainingProblem = {
     implicit val requstFormatter = jsonFormat2(TrainingRequest)
+    implicit val resultFormatter = jsonFormat4(TrainingProblem)
 
-    val request = TrainingRequest(size, operators)
     val requestJson = request.toJson.compactPrint
-
     val result = Http(url(BaseUrl + "train").POST <<? params << requestJson OK as.String)
 
     result().asJson.convertTo[TrainingProblem]
