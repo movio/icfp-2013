598ed602fb6c9d8545d374fe69482a86599971b3
kalmanb <kalman@movio.co>
2013-08-10 01:19:35 +1200

    Add remote access for Training

---
+++ src/main/scala/Remote.scala
@@ -0,0 +1,47 @@
+
+import dispatch._
+import dispatch.Defaults._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
+object Remote extends App {
+  val BaseUrl = "http://icfpc2013.cloudapp.net/"
+  val params = Map("auth" -> "0328SAmjHevv7SW0OT2lYdIxjSuhVp2HX31j1dSSvpsH1H")
+
+  case class TrainingProblem(
+    challenge: String,
+    id: String,
+    size: Int,
+    operators: Seq[String])
+
+  def getTrainer(size: Option[Int] = None, operators: Option[Seq[String]] = None): TrainingProblem = {
+    implicit val resultFormatter = jsonFormat4(TrainingProblem)
+    case class TrainingRequest(
+      size: Option[Int],
+      operators: Option[Seq[String]])
+    implicit val requstFormatter = jsonFormat2(TrainingRequest)
+
+    val request = TrainingRequest(size, operators)
+    val requestJson = request.toJson.compactPrint
+
+    val result = Http(url(BaseUrl + "train").POST <<? params << requestJson OK as.String)
+
+    result().asJson.convertTo[TrainingProblem]
+  }
+
+  case class EvalRequest(
+    id: Option[String],
+    program: Option[String],
+    arguments: Seq[String])
+
+  case class EvalResponse(
+    status: String,
+    outputs: Option[Seq[String]],
+    message: Option[String])
+
+  def eval(request: EvalRequest): EvalResponse = {
+    ???
+  }
+
+}
+
