32a125ea3c08cb20cc754c5b1dbe5ac353dfc01d
Jonathan Chow <jonathan@movio.co>
2013-08-09 23:41:37 +1200

    reorganised

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -50,19 +50,19 @@ trait Parser extends RegexParsers {
 
   def op1 = ("(" ~> ("not" | "shl1" | "shr16" | "shr1" | "shr4") ~ expr <~ ")") ^^ {
     case opName ~ e ⇒ opName match {
-      case "not" ⇒ Not(e)
-      case "shl1" ⇒ Shl1(e)
-      case "shr1" ⇒ Shr1(e)
-      case "shr4" ⇒ Shr4(e)
+      case "not"   ⇒ Not(e)
+      case "shl1"  ⇒ Shl1(e)
+      case "shr1"  ⇒ Shr1(e)
+      case "shr4"  ⇒ Shr4(e)
       case "shr16" ⇒ Shr16(e)
     }
   }
 
   def op2 = ("(" ~> ("and" | "or" | "xor" | "plus") ~ expr ~ expr <~ ")") ^^ {
     case opName ~ e1 ~ e2 ⇒ opName match {
-      case "and" ⇒ And(e1, e2)
-      case "or" ⇒ Or(e1, e2)
-      case "xor" ⇒ Xor(e1, e2)
+      case "and"  ⇒ And(e1, e2)
+      case "or"   ⇒ Or(e1, e2)
+      case "xor"  ⇒ Xor(e1, e2)
       case "plus" ⇒ Plus(e1, e2)
     }
   }
@@ -72,23 +72,57 @@ trait Parser extends RegexParsers {
   def parse(s: String) = parseAll(P, s)
 }
 
-object Program extends Parser with App {
+object Program extends Parser with App { tests()
+
+  // hack around below here
+
+  eval(
+    0x1122334455667788L,
+    "(lambda (x) (fold x 0 (lambda (y z) (if0 y y y))))"
+  )
+
+  eval(
+    0x01L,
+    "(lambda (x) (plus x 1))"
+  )
+
+
+
+
+  // hack around above here
+
+
+  def eval(input: Long, prog: String): Long = {
+    println
+    print("in:   ")
+    output(input)
+
+    print("prog: ")
+    println(prog)
+
+    val result = evaluate(parseAll(P, prog).get, Map("x" -> input))
+    print("out:  ")
+    output(result)
+    println
+
+    result
+  }
 
   def evaluate(expr: Expr, env: Map[String, Long]): Long = expr match {
-    case Value(v) ⇒ v
-    case Id(id) ⇒ env(id)
+    case Value(v)   ⇒ v
+    case Id(id)     ⇒ env(id)
 
     // op1
-    case Not(e) ⇒ ~(evaluate(e, env))
-    case Shl1(e) ⇒ (evaluate(e, env))  << 1
-    case Shr1(e) ⇒ (evaluate(e, env))  >> 1
-    case Shr4(e) ⇒ (evaluate(e, env))  >> 4
-    case Shr16(e) ⇒ (evaluate(e, env)) >> 16
+    case Not(e)     ⇒ ~(evaluate(e, env))
+    case Shl1(e)    ⇒ (evaluate(e, env)) << 1
+    case Shr1(e)    ⇒ (evaluate(e, env)) >> 1
+    case Shr4(e)    ⇒ (evaluate(e, env)) >> 4
+    case Shr16(e)   ⇒ (evaluate(e, env)) >> 16
 
     // op2
-    case And(l, r) ⇒ evaluate(l, env)  & evaluate(r, env)
-    case Or(l, r) ⇒ evaluate(l, env)   | evaluate(r, env)
-    case Xor(l, r) ⇒ evaluate(l, env)  ^ evaluate(r, env)
+    case And(l, r)  ⇒ evaluate(l, env) & evaluate(r, env)
+    case Or(l, r)   ⇒ evaluate(l, env) | evaluate(r, env)
+    case Xor(l, r)  ⇒ evaluate(l, env) ^ evaluate(r, env)
     case Plus(l, r) ⇒ evaluate(l, env) + evaluate(r, env)
 
     case If0(p, t, f) ⇒
@@ -96,45 +130,48 @@ object Program extends Parser with App {
       else evaluate(f, env)
 
     case Fold(e, i, l) ⇒
-      val (num, init)  = (evaluate(e, env), evaluate(i, env))
+      val (num, init) = (evaluate(e, env), evaluate(i, env))
       (0 to 7).foldLeft(init) { (memo: Long, shiftFactor: Int) ⇒
         val numPart: Long = (num >> shiftFactor * 8) & 0xFF
         evaluate(l, env ++ Map(l.id1.id → numPart, l.id2.id → memo))
       }
 
     // lambda
-    case Lambda1(_, b) ⇒ (evaluate(b, env))
+    case Lambda1(_, b)    ⇒ (evaluate(b, env))
     case Lambda2(_, _, b) ⇒ (evaluate(b, env))
   }
 
-  assert(1     == print(evaluate(parseAll(expr, "x").get, Map("x" →    1L))))
-  assert(1     == print(evaluate(parseAll(expr, "1").get, Map())))
-  assert(-2    == print(evaluate(parseAll(expr, "(not 1)").get, Map())))
-  assert(2     == print(evaluate(parseAll(expr, "(shl1 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(shr1 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(shr16 1)").get, Map())))
-  assert(64    == print(evaluate(parseAll(P, "(lambda (x) x)").get, Map("x" → 64L))))
-  assert(2     == print(evaluate(parseAll(P, "(lambda (x) (shr16 x))").get, Map("x" → Math.pow(2, 17).toLong))))
-  assert(1     == print(evaluate(parseAll(expr, "(and 1 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(and 1 0)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(and 1 (shr16 1))").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(and 0 1)").get, Map())))
-  assert(1     == print(evaluate(parseAll(expr, "(or 0 1)").get, Map())))
-  assert(1     == print(evaluate(parseAll(expr, "(or (shr4 1) 1)").get, Map())))
-  assert(1     == print(evaluate(parseAll(expr, "(xor (shr4 1) 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(xor 1 1)").get, Map())))
-  assert(2     == print(evaluate(parseAll(expr, "(plus 1 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(if0 1 1 0)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(if0 0 0 1)").get, Map())))
-  assert(0     == print(evaluate(parseAll(expr, "(fold 0 0 (lambda (x y) (and x y)))").get, Map())))
-  assert(0xFF  == print(evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (or y z))))").get, Map("x" → 0x1122334455667788L))))
-  assert(36    == print(evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (plus y z))))").get, Map("x" →  0x0102030405060708L))))
-  assert(0xFF  == print(evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (if0 y y y))))").get, Map("x" → 0xFF00000000000000L))))
-  assert(0     == print(evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (if0 y y y))))").get, Map("x" → 0x00000000000000FFL))))
-
-  def print(long: Long) = {
+  def output(long: Long) = {
+    val str = long.toBinaryString
+    for(_ ← 1 to (64 - str.length)) { print("0") }
     println(long.toBinaryString)
     long
   }
 
+  def tests(): Unit = {
+    assert(1 == evaluate(parseAll(expr, "x").get, Map("x" → 1L)))
+    assert(1 == evaluate(parseAll(expr, "1").get, Map()))
+    assert(-2 == evaluate(parseAll(expr, "(not 1)").get, Map()))
+    assert(2 == evaluate(parseAll(expr, "(shl1 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(shr1 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(shr16 1)").get, Map()))
+    assert(64 == evaluate(parseAll(P, "(lambda (x) x)").get, Map("x" → 64L)))
+    assert(2 == evaluate(parseAll(P, "(lambda (x) (shr16 x))").get, Map("x" → Math.pow(2, 17).toLong)))
+    assert(1 == evaluate(parseAll(expr, "(and 1 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(and 1 0)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(and 1 (shr16 1))").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(and 0 1)").get, Map()))
+    assert(1 == evaluate(parseAll(expr, "(or 0 1)").get, Map()))
+    assert(1 == evaluate(parseAll(expr, "(or (shr4 1) 1)").get, Map()))
+    assert(1 == evaluate(parseAll(expr, "(xor (shr4 1) 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(xor 1 1)").get, Map()))
+    assert(2 == evaluate(parseAll(expr, "(plus 1 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(if0 1 1 0)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(if0 0 0 1)").get, Map()))
+    assert(0 == evaluate(parseAll(expr, "(fold 0 0 (lambda (x y) (and x y)))").get, Map()))
+    assert(0xFF == evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (or y z))))").get, Map("x" → 0x1122334455667788L)))
+    assert(36 == evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (plus y z))))").get, Map("x" → 0x0102030405060708L)))
+    assert(0xFF == evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (if0 y y y))))").get, Map("x" → 0xFF00000000000000L)))
+    assert(0 == evaluate(parseAll(P, "(lambda (x) (fold x 0 (lambda (y z) (if0 y y y))))").get, Map("x" → 0x00000000000000FFL)))
+  }
 }
