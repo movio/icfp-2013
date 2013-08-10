6f1887ebe4f036d6f950200953a3e91bad8b2445
Stefan Bleibinhaus <stefan@movio.co>
2013-08-10 12:10:52 +1200

    some convenient methods to work with Hex

---
+++ src/main/scala/Hex.scala
@@ -0,0 +1,14 @@
+import scala.util.Random
+import java.math.BigInteger
+
+object Hex {
+  val sample = randomSample()
+  
+  def randomHexString() = "0x" + Random.nextLong.toHexString
+  def randomSample() = (1 to 256).map(_ => randomHexString())
+  def hexToLong(hex: String) = new BigInteger(hex.substring(2), 16).longValue()
+  def longToHex(l: Long) = "0x" + longWrapper(l).toHexString
+  
+  //println(hexToLong("0xffffffffffffffff"))
+  //println(longToHex(new BigInteger("ffffffffffffffff", 16).longValue()))
+}
